7b2438fced27b6ed042f7e50ed0961331e705f08
jing-tw <mqjing@gmail.com>
2016-06-26 01:52:37 +0800

    Ubuntu 15.10: OK & add flush multipath code in mount Download folder

--- install_scst/03_make_scst_and_add_module.sh
+++ install_scst/03_make_scst_and_add_module.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# flush unsed multipath (when the vagrant mount share folder failure by updated guest kernel)
+# show  the current multipath topology
+sudo multipath -ll
+
+# flush all unused multipath device maps
+sudo multipath -F
+
+# check the multipath topology
+sudo multipath -ll
+
 # remount sdb1 again to see the Download
 cd ~/Downloads
 DownloadPath=`pwd`
